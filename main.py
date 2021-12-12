03c077e8205fb3347e91a4538a317c39e33994bb
Kelvin Wong <kelvinjjwong@outlook.com>
2021-12-12 13:19:09 +0800

    add flask and apsecheduler; add a poc for integration

--- main.py
+++ main.py
@@ -3,14 +3,51 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+from flask import Flask
+from apscheduler.schedulers.background import BackgroundScheduler
+
+def job():
+	print('job running')
+
+scheduler = BackgroundScheduler()
+scheduler.start()
+
+scheduler.add_job(
+	job,
+	trigger='interval',
+	seconds=5,
+    id='face_job'
+)
+
+app = Flask(__name__)
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+@app.route("/stop")
+def stopJob():
+    scheduler.pause_job('face_job')
+    return "stopped"
+
+@app.route("/start")
+def startJob():
+    scheduler.resume_job('face_job')
+    return "started"
+
+@app.route("/list/jobs")
+def printJobs():
+    scheduler.print_jobs()
+    return "printed job list"
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
---
+++ update_conda_yml.sh
@@ -0,0 +1,2 @@
+conda env export | egrep -v "^prefix:" | egrep -v "^name:" > conda_env_python371.yml
+
