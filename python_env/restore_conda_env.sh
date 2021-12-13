774da255b6f8170fdb4942b53a62f2e78305d57a
Kelvin Wong <kelvinjjwong@outlook.com>
2021-12-13 11:59:53 +0800

    move scripts to dedicated folder

--- python_env/restore_conda_env.sh
+++ python_env/restore_conda_env.sh
@@ -1,4 +1,9 @@
-ENV_NAME=face37
+source conda_env.conf
+
+conda --version
+if [[ $? -ne 0 ]]; then
+  exit 1;
+fi
 
 if [[ ! -e ~/.pip/pip.conf ]]; then
   source ./setup_pip_source.sh
@@ -6,7 +11,9 @@ fi
 
 exists=`conda env list | grep $ENV_NAME`
 if [[ "$exists" != "" ]]; then
-  conda env update -n $ENV_NAME -f ../conda_env_python371.yml
+  conda env update -n $ENV_NAME -f conda_env_python371.yml
 else
-  conda env create -n $ENV_NAME -f ../conda_env_python371.yml
+  conda env create -n $ENV_NAME -f conda_env_python371.yml
 fi
+
+conda activate $ENV_NAME
--- python_env/update_conda_yml.sh
+++ python_env/update_conda_yml.sh
@@ -1,2 +1,12 @@
-conda env export | egrep -v "^prefix:" | egrep -v "^name:" > ../conda_env_python371.yml
+source conda_env.conf
+
+conda --version
+if [[ $? -ne 0 ]]; then
+  exit 1;
+fi
+
+conda deactivate
+conda activate $ENV_NAME
+
+conda env export | egrep -v "^prefix:" | egrep -v "^name:" > conda_env_python371.yml
 
